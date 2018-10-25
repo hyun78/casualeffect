d41275a4e29d9f53a4a778b8e0265ca6ea3c7653
hyun <hyun@hyunui-MacBook-Pro-2.local>
2018-10-25 15:07:35 +0900

    dependency setting

--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup
 
 setup(name='casualeffect',
       version='0.1',
-      description='Measuring casual effect in given data',
+      description='Python framework that measuring casual effect',
       url='https://github.com/hyun78/casualeffect',
       author='HyeonHo Song',
       author_email='hyun78@kaist.ac.kr',
       license='MIT',
       packages=['casualeffect'],
+	  install_requires=[
+          'pandas',
+		  'gensim',
+		  'scikit-learn',
+      ],
       zip_safe=False)
 
